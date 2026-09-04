980667a0a2286f851e2513f0ae93f922c83ba106
CAETANO CORDEIRO DOS SANTOS NETO <caetano.neto@senaisp.edu.br>
2026-09-04 09:33:58 -0300

    olá

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$nome = "Caetano Cordeiro dos Santos Neto";
+$profissao = "Automação de processo";
+$mensagem = "É o vibe e o codas não tem jeito";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP - Carol Souza</title>
+</head>
+<body>
+
+    <h1><?php echo $nome; ?></h1>
+    
+    <h2><?php echo $profissao; ?></h2>
+
+    <p><?php echo $mensagem; ?></p>
+
+    <img src=" " alt="">
+    
+</body>
+</html>
